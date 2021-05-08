8f9360c68097289a5c4ca9c937b8c9053b89c831
Ilija Brdar <ilija_brdar@yahoo.com>
2021-05-08 18:45:10 +0200

    integration with the drools project

--- back-end/src/main/java/com/example/backend/BackEndApplication.java
+++ back-end/src/main/java/com/example/backend/BackEndApplication.java
@@ -1,7 +1,11 @@
 package com.example.backend;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackEndApplication {
@@ -10,4 +14,13 @@ public class BackEndApplication {
 		SpringApplication.run(BackEndApplication.class, args);
 	}
 
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+	}
 }
